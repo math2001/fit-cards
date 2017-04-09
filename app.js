91e42ab88954ba4b0c4c5992b4dd529f0a0c7ef9
Mathieu PATUREL <australie.p@gmail.com>
2017-04-09 16:55:47 +1000

    :construction: 'use' buttons working for preset decks

--- app.js
+++ app.js
@@ -2,21 +2,44 @@
 
 class App {
 
-    static renderOptions(decks) {
+    static updateDecks(decks) {
+        this.decks = decks
         let html = ''
-        decks.some((deck) => {
+        decks.some((deck, index) => {
             html += `<tr>`
             deck.some((item) => {
                 html += `<td>${item}</td>`
             })
-            html += `<td><button>Use!</button></td>`
+            html += `<td><button class="use-deck" data-deck-index="${index}">Use!</button></td>`
             html += `</tr>`
         })
         document.querySelector('#groups').innerHTML = html
     }
 
+    static use(deckIndex) {
+        [this.heart, this.spade, this.club, this.diamond].some((element, index) => {
+            element.textContent = this.decks[deckIndex][index + 1]
+        })
+    }
+
+    static cacheDOM() {
+        this.heart = document.querySelector('#heart')
+        this.spade = document.querySelector('#spade')
+        this.club = document.querySelector('#club')
+        this.diamond = document.querySelector('#diamond')
+        this.decksPopup = document.querySelector('#decks')
+    }
+
     static app() {
-        fetch('decks.json').then(responce => responce.json()).then(decks => this.renderOptions(decks))
+        this.cacheDOM()
+        fetch('decks.json').then(responce => responce.json()).then(decks => this.updateDecks(decks))
+
+        document.body.addEventListener('click', e => {
+            if (!e.target.classList.contains('use-deck')) { return }
+            this.use(parseInt(e.target.getAttribute('data-deck-index')))
+            this.decksPopup.classList.remove('active')
+        })
+
     }
 
 }
